77b85f6e7cb88507aac7bb29b8ba980cbf8031c2
JOY <anh@metados.com>
2025-05-11 20:38:02 +0700

    Register all custom fields including country-select, currency-select, etc.

--- server/src/register.ts
+++ server/src/register.ts
@@ -1,9 +1,44 @@
 const register = ({ strapi }: { strapi: any }) => {
+  // Đăng ký custom field address-autocomplete
   strapi.customFields.register({
     name: 'address-autocomplete',
     plugin: 'locale-select',
     type: 'string',
-    // Có thể bổ sung thêm các option khác nếu cần thiết
+  });
+  
+  // Đăng ký custom field country-select
+  strapi.customFields.register({
+    name: 'country-select',
+    plugin: 'locale-select',
+    type: 'string',
+  });
+  
+  // Đăng ký custom field currency-select
+  strapi.customFields.register({
+    name: 'currency-select',
+    plugin: 'locale-select',
+    type: 'string',
+  });
+  
+  // Đăng ký custom field language-select
+  strapi.customFields.register({
+    name: 'language-select',
+    plugin: 'locale-select',
+    type: 'string',
+  });
+  
+  // Đăng ký custom field timezone-select
+  strapi.customFields.register({
+    name: 'timezone-select',
+    plugin: 'locale-select',
+    type: 'string',
+  });
+  
+  // Đăng ký custom field location-select
+  strapi.customFields.register({
+    name: 'location-select',
+    plugin: 'locale-select',
+    type: 'string',
   });
 };
 export default register;
