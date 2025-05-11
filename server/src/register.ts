4665005c31bface3804da4113ec6d54ccdb132ad
JOY <anh@metados.com>
2025-05-11 09:39:34 +0700

    fix: register custom field address-autocomplete

--- server/src/register.ts
+++ server/src/register.ts
@@ -1,3 +1,9 @@
-// Đã xóa đăng ký custom field phía server để tránh lỗi invariant
-const register = () => {};
+const register = ({ strapi }: { strapi: any }) => {
+  strapi.customFields.register({
+    name: 'address-autocomplete',
+    plugin: 'locale-select',
+    type: 'string',
+    // Có thể bổ sung thêm các option khác nếu cần thiết
+  });
+};
 export default register;
